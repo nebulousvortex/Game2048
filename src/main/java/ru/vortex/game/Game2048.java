2bdafbbddf2f3f594cf994df4c8e3813e4349081
NebulousVortex <VortexOfNebula@mail.ru>
2023-12-03 12:47:43 +0300

    Added realization of the move() method

--- src/main/java/ru/vortex/game/Game2048.java
+++ src/main/java/ru/vortex/game/Game2048.java
@@ -1,46 +1,101 @@
 package main.java.ru.vortex.game;
 
+import main.java.ru.vortex.assistants.Key;
 import main.java.ru.vortex.board.Board;
 import main.java.ru.vortex.assistants.Direction;
 import main.java.ru.vortex.assistants.GameHelper;
+import main.java.ru.vortex.board.SquareBoard;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.ListIterator;
 import java.util.Random;
 
+import static java.util.Collections.*;
+import static java.util.Collections.reverse;
+
 public class Game2048 implements Game {
-    private GameHelper helper = new GameHelper();
-    private Board board;
+    private GameHelper helper;
+    private SquareBoard board;
     private Random random = new Random();
-    public Game2048(Board board) {
+    public Game2048(SquareBoard board) {
         this.board = board;
+        this.init();
     }
 
+    /*
+     *initializes the board and the random
+     */
     @Override
-    public void init() {
-        //this.board.fillBoard();
+    public void init(){
+        helper = new GameHelper();
+        random = new Random();
     }
 
+    /*
+     *Checks for the absence of empty values
+     */
     @Override
     public boolean canMove() {
-        return false;
+        return (!board.availableSpace().isEmpty());
     }
 
+    /*
+     *Moves and adds values
+     */
     @Override
-    public boolean move(Direction direction) {
-        return false;
+    public void move(Direction direction) {
+        if (direction == Direction.RIGHT){
+            for(int i = 0; i<board.getHeight(); i++){
+                List<Integer> values = helper.moveAndMergeEqual(board.getValueRow(i));
+                reverse(values);
+            }
+        }
+        if (direction == Direction.LEFT){
+            for(int i = 0; i<board.getHeight(); i++){
+                helper.moveAndMergeEqual(board.getValueRow(i));
+            }
+        }
+        if (direction == Direction.UP){
+            for(int i = 0; i<board.getWidth(); i++){
+                helper.moveAndMergeEqual(board.getValueColumn(i));
+            }
+        }
+        if (direction == Direction.DOWN){
+            for(int i = 0; i<board.getWidth(); i++){
+                List<Integer> values = helper.moveAndMergeEqual(board.getValueColumn(i));
+                reverse(values);
+            }
+        }
     }
 
+    /*
+     *Fills random empty values with a random number
+     */
     @Override
     public void addItem() {
-
+        List<Key> spaces = board.availableSpace();
+        ListIterator<Key> listIterator = spaces.listIterator();
+        while(listIterator.hasNext()) {
+            if (random.nextFloat() > 0.7f) {
+                board.setBoard(listIterator.next(), random.nextInt(16));
+            }
+        }
     }
 
+    /*
+     *Returns the board
+     */
     @Override
     public Board getGameBoard() {
-        return null;
+        return board;
     }
 
+    /*
+     *Checks for the presence of 2048 in the values
+     */
     @Override
     public boolean hasWin() {
-        return false;
+        return board.hasValue(2048);
     }
 }
