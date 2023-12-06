a3de53cff4da5a19ae86de3b06296caaf2891a7e
NebulousVortex <VortexOfNebula@mail.ru>
2023-12-06 13:29:03 +0300

    Few small changes

--- src/main/java/ru/vortex/test/TestClass.java
+++ src/main/java/ru/vortex/test/TestClass.java
@@ -9,7 +9,7 @@ import java.util.*;
 
 public class TestClass {
     public static void main(String[] args){
-        SquareBoard board= new SquareBoard(4);
+        SquareBoard board = new SquareBoard(4);
         Game game2048 = new Game2048(board);
         System.out.println(game2048.canMove());
     }
