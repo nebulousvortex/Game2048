8aaad16891135699c918960de9290198fc791339
NebulousVortex <VortexOfNebula@mail.ru>
2023-12-08 23:42:13 +0300

    Few small changes

--- src/main/java/ru/vortex/test/TestClass.java
+++ src/main/java/ru/vortex/test/TestClass.java
@@ -7,7 +7,7 @@ import main.java.ru.vortex.game.Game2048;
 public class TestClass {
     public static void main(String[] args){
         SquareBoard board = new SquareBoard(4);
-        Game game2048 = new Game2048(board);
+        Game game2048 = new Game2048();
         System.out.println(game2048.canMove());
     }
 
